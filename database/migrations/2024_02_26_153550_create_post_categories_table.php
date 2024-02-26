0386c2e0b881e5a9332387606ffd4111f177e9dc
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-26 19:06:29 +0330

    post_categories migration

---
+++ database/migrations/2024_02_26_153550_create_post_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug')->unique()->nullable();
+            $table->text('image');
+            $table->tinyInteger('status')->default(0);
+            $table->string('tags');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_categories');
+    }
+}
