f481ef4fe3d8558cc6b207dcae738b2814453811
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-26 19:44:15 +0330

    posts migration

---
+++ database/migrations/2024_02_26_153842_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique()->nullable();
+            $table->text('summary');
+            $table->text('body');
+            $table->text('image');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('commentable')->default(0)->comment('0=>uncommentable , 1=>commentable');
+            $table->string('tags');
+            $table->timestamp('published_at');
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('post_categories');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
