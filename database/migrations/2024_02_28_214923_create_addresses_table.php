546ea949a9f55bb0ac8ba2af82fa7dcad2c79b56
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-29 01:35:45 +0330

    proviences , cities, addresses , delivery migration

---
+++ database/migrations/2024_02_28_214923_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('city_id')->nullable()->constrained('cities')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('no');
+            $table->string('unit');
+            $table->string('recipient_first_name');
+            $table->string('recipient_last_name');
+            $table->string('mobile');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
---
+++ database/migrations/2024_02_28_215023_create_delivery_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('amount', 20, 3)->nullable();
+            $table->integer('delivery_time')->nullable();
+            $table->string('delivery_time_unit')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery');
+    }
+}
