07360c8f241e7e7fb184af7217e38c52eb9259bf
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-29 15:06:03 +0330

    offline_payments migration

---
+++ database/migrations/2024_02_29_075821_create_offline_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfflinePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offline_payments', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 20, 3);
+            $table->foreignId('user_id')->nullable()->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('transaction_id')->nullable();
+            $table->timestamp('pay_date');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offline_payments');
+    }
+}
