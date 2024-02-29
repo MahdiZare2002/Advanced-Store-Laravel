8705f2975963028852991b0993b4ed2fe297dd88
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-29 15:13:05 +0330

    online_payments migration

--- database/migrations/2024_02_29_081055_create_online_payments_table.php
+++ database/migrations/2024_02_29_081055_create_online_payments_table.php
@@ -15,7 +15,15 @@ class CreateOnlinePaymentsTable extends Migration
     {
         Schema::create('online_payments', function (Blueprint $table) {
             $table->id();
+            $table->decimal('amount', 20, 3);
+            $table->foreignId('user_id')->nullable()->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamp('getaway');
+            $table->string('transaction_id')->nullable();
+            $table->string('bank_first_response')->nullable();
+            $table->string('bank_second_response')->nullable();
+            $table->tinyInteger('status')->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
