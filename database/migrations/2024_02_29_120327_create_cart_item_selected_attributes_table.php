3edee56ca7e305c5f72d3c6149767d8469740fa9
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-02-29 15:47:37 +0330

    cart_item_selected_attributes migration

--- database/migrations/2024_02_29_120327_create_cart_item_selected_attributes_table.php
+++ database/migrations/2024_02_29_120327_create_cart_item_selected_attributes_table.php
@@ -15,7 +15,12 @@ class CreateCartItemSelectedAttributesTable extends Migration
     {
         Schema::create('cart_item_selected_attributes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_item_id')->constrained('cart_items')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('category_attribute_id')->constrained('category_attributes')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('category_value_id')->constrained('category_values')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('value')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
