d05fcd57205547661d315b18484a1c3a91fc6641
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-01 21:12:43 +0330

    create blade complated

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -1,63 +1,97 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-<title>دسته بندی</title>
+    <title>دسته بندی</title>
 @endsection
 
 @section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد دسته بندی</li>
+        </ol>
+    </nav>
 
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
-      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد دسته بندی</li>
-    </ol>
-  </nav>
+
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header">
+                    <h5>
+                        ایجاد دسته بندی
+                    </h5>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                    <a href="{{ route('admin.content.category.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                </section>
+
+                <section>
+                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <section class="row">
+
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="name">نام دسته</label>
+                                    <input type="text" class="form-control form-control-sm" name="name"
+                                        id="name">
+                                </div>
+                            </section>
+
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="tags">تگ ها</label>
+                                    <input type="text" class="form-control form-control-sm" name="tags"
+                                        id="tags">
+                                </div>
+                            </section>
+
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select name="status" id="" class="form-control form-control-sm"
+                                        id="status">
+                                        <option value="0">غیرفعال</option>
+                                        <option value="1">فعال</option>
+                                    </select>
+                                </div>
+                            </section>
+
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="image">تصویر</label>
+                                    <input type="file" class="form-control form-control-sm" name="image"
+                                        id="image">
+                                </div>
+                            </section>
 
 
-  <section class="row">
-    <section class="col-12">
-        <section class="main-body-container">
-            <section class="main-body-container-header">
-                <h5>
-                  ایجاد دسته بندی
-                </h5>
-            </section>
+                            <section class="col-12">
+                                <div class="form-group">
+                                    <label for="">توضیحات</label>
+                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6"></textarea>
+                                </div>
+                            </section>
 
-            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{ route('admin.content.category.index') }}" class="btn btn-info btn-sm">بازگشت</a>
-            </section>
 
-            <section>
-                <form action="" method="">
-                    <section class="row">
-
-                        <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">نام دسته</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
+                            <section class="col-12">
+                                <button class="btn btn-primary btn-sm">ثبت</button>
+                            </section>
                         </section>
+                    </form>
+                </section>
 
-                        <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">دسته والد</label>
-                                <select name="" id="" class="form-control form-control-sm">
-                                    <option value="">دسته را انتخاب کنید</option>
-                                    <option value="">وسایل الکترونیکی</option>
-                                </select>
-                            </div>
-                        </section>
-                        <section class="col-12">
-                            <button class="btn btn-primary btn-sm">ثبت</button>
-                        </section>
-                    </section>
-                </form>
             </section>
-
         </section>
     </section>
-</section>
+@endsection
 
+@section('script')
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
 @endsection
