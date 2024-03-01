bc59d30da2f6fdf14c91b24ce9dad6e405388945
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-02 00:40:42 +0330

    category.create errors handle

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -33,51 +33,90 @@
                         @csrf
                         <section class="row">
 
-                            <section class="col-12 col-md-6">
+                            <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="name">نام دسته</label>
-                                    <input type="text" class="form-control form-control-sm" name="name"
-                                        id="name">
+                                    <input type="text" class="form-control form-control-sm" name="name" id="name"
+                                        value="{{ old('name') }}">
                                 </div>
+                                @error('name')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
 
-                            <section class="col-12 col-md-6">
+                            <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
-                                    <input type="text" class="form-control form-control-sm" name="tags"
-                                        id="tags">
+                                    <input type="text" class="form-control form-control-sm" name="tags" id="tags"
+                                        value="{{ old('tags') }}">
                                 </div>
+                                @error('tags')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
 
-                            <section class="col-12 col-md-6">
+                            <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select name="status" id="" class="form-control form-control-sm"
                                         id="status">
-                                        <option value="0">غیرفعال</option>
-                                        <option value="1">فعال</option>
+                                        <option value="0" @if (old('status') == 0) selected @endif>غیرفعال
+                                        </option>
+                                        <option value="1" @if (old('status') == 1) selected @endif>فعال
+                                        </option>
                                     </select>
                                 </div>
+                                @error('status')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
 
-                            <section class="col-12 col-md-6">
+                            <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="image">تصویر</label>
                                     <input type="file" class="form-control form-control-sm" name="image"
                                         id="image">
                                 </div>
+                                @error('image')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
 
 
                             <section class="col-12">
                                 <div class="form-group">
                                     <label for="">توضیحات</label>
-                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6"></textarea>
+                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6">
+                                    {{ old('description') }}
+                                </textarea>
                                 </div>
+                                @error('description')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
 
 
-                            <section class="col-12">
+                            <section class="col-12 my-3">
                                 <button class="btn btn-primary btn-sm">ثبت</button>
                             </section>
                         </section>
