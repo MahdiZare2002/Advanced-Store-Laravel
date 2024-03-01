c797244ea8b13078c2722845fa797212d976514d
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-02 01:27:13 +0330

    ContentCategory update

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -65,7 +65,7 @@ class CategoryController extends Controller
      */
     public function edit(PostCategory $postCategory)
     {
-        dd($postCategory);
+        return view('admin.content.category.edit', compact('postCategory'));
     }
 
     /**
@@ -75,9 +75,12 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostCategoryRequest $request, PostCategory $postCategory)
     {
-        //
+        $inputs = $request->all();
+        $inputs['image'] = 'image';
+        $postCategory->update($inputs);
+        return redirect()->route('admin.content.category.index');
     }
 
     /**
