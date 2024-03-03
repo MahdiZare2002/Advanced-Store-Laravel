164e115214a425007cef87218ec88b11a165509d
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-03 23:48:12 +0330

    add select2 to project

---
+++ resources/views/admin/content/category/edit.blade.php
@@ -0,0 +1,138 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>دسته بندی</title>
+@endsection
+
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد دسته بندی</li>
+        </ol>
+    </nav>
+
+
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header">
+                    <h5>
+                        ایجاد دسته بندی
+                    </h5>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                    <a href="{{ route('admin.content.category.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                </section>
+
+                <section>
+                    <form action="{{ route('admin.content.category.update', $postCategory->id) }}" method="post"
+                        enctype="multipart/form-data">
+                        @csrf
+                        {{ method_field('put') }}
+                        <section class="row">
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="name">نام دسته</label>
+                                    <input type="text" class="form-control form-control-sm" name="name" id="name"
+                                        value="{{ old('name', $postCategory->name) }}">
+                                </div>
+                                @error('name')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="tags">تگ ها</label>
+                                    <input type="text" class="form-control form-control-sm" name="tags" id="tags"
+                                        value="{{ old('tags', $postCategory->tags) }}">
+                                </div>
+                                @error('tags')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select name="status" id="" class="form-control form-control-sm"
+                                        id="status">
+                                        <option value="0" @if (old('status', $postCategory->status) == 0) selected @endif>غیرفعال
+                                        </option>
+                                        <option value="1" @if (old('status', $postCategory->status) == 1) selected @endif>فعال
+                                        </option>
+                                    </select>
+                                </div>
+                                @error('status')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="image">تصویر</label>
+                                    <input type="file" class="form-control form-control-sm" name="image"
+                                        id="image">
+                                </div>
+                                @error('image')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+
+                            <section class="col-12">
+                                <div class="form-group">
+                                    <label for="">توضیحات</label>
+                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6">
+                                    {{ old('description', $postCategory->description) }}
+                                </textarea>
+                                </div>
+                                @error('description')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+
+                            <section class="col-12 my-3">
+                                <button class="btn btn-primary btn-sm">ثبت</button>
+                            </section>
+                        </section>
+                    </form>
+                </section>
+
+            </section>
+        </section>
+    </section>
+@endsection
+
+@section('script')
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
+@endsection
