8577491f0fab16691b374d037cbeab1b51540463
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-06 01:38:56 +0330

    add select2 scripts

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -136,4 +136,32 @@
     <script>
         CKEDITOR.replace('description');
     </script>
+
+    <script>
+        $(document).ready(function() {
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if (tags_input.val() !== null && tags_input.val().length > 0) {
+                default_data = default_tags.split(',');
+            }
+
+            select_tags.select2({
+                placeholder: 'لطفا تگ های خود را وارد نمایید',
+                tags: true,
+                data: default_data
+            });
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function(event) {
+                if (select_tags.val() !== null && select_tags.val().length > 0) {
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource)
+                }
+            })
+        })
+    </script>
 @endsection
