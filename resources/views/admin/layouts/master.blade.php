b0ab046d7556fc6424b8561b98804fd4721bb228
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-06 23:29:28 +0330

    alerts bux fixes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,35 +4,38 @@
 <head>
     @include('admin.layouts.head-tag')
     @yield('head-tag')
+
 </head>
 
 <body dir="rtl">
 
-
     @include('admin.layouts.header')
+
 
 
     <section class="body-container">
 
         @include('admin.layouts.sidebar')
 
+
         <section id="main-body" class="main-body">
 
-
             @yield('content')
-
 
         </section>
     </section>
 
 
-
-
-
-
-
     @include('admin.layouts.script')
     @yield('script')
+
+    <section class="toast-wrapper flex-row-reverse">
+        @include('admin.alerts.toast.success')
+        @include('admin.alerts.toast.error')
+    </section>
+
+    @include('admin.alerts.sweetalert.error')
+    @include('admin.alerts.sweetalert.success')
 
 
 </body>
