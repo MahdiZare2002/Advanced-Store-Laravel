8084b6392a3a5112fca41ff9195c497276206155
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-08 02:20:11 +0330

    create ImageSerives

---
+++ app/Http/Services/Image/ImageToolsService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Services\Image;
+
+class ImageToolsService {
+
+    protected $image;
+    protected $exclusiveDirectory;
+    protected $imageDirectory;
+    protected $imageName;
+    protected $imageFormat;
+    protected $finalImageDirectory;
+    protected $finalImageName;
+
+
+}
