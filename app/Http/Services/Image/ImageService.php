c3fc78e32afa1dd4ce726c32aff14d188b1a2f00
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-08 14:53:43 +0330

    fitAndSave image method

--- app/Http/Services/Image/ImageService.php
+++ app/Http/Services/Image/ImageService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services\Image;
 
 use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImageService extends ImageToolsService
 {
@@ -16,7 +17,25 @@ class ImageService extends ImageToolsService
         $this->provider();
 
         //save Image
-        $result = ImageManager::read($image->getRealPath())->save(public_path($this->getImageAddress()), null, $this->getImageFormat());
+        $manager = new ImageManager(new Driver());
+
+        $result = $manager->read($image->getRealPath())->save(public_path($this->getImageAddress()), null, $this->getImageFormat());
+
+        return $result ? $this->getImageAddress() : false;
+    }
+
+    public function fitAndSave($image, $width, $height)
+    {
+        //set image
+        $this->setImage($image);
+
+        //execute provider
+        $this->provider();
+
+        //save Image
+        $manager = new ImageManager(new Driver());
+
+        $result = $manager->read($image->getRealPath())->cover($width, $height)->save(public_path($this->getImageAddress()), null, $this->getImageFormat());
 
         return $result ? $this->getImageAddress() : false;
     }
