1483aa554406627c805e187b21f37412ce164c72
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-09 14:17:27 +0330

    add deletedir to service

--- app/Http/Services/Image/ImageService.php
+++ app/Http/Services/Image/ImageService.php
@@ -89,4 +89,28 @@ class ImageService extends ImageToolsService
             unlink($imagePath);
         }
     }
+
+    public function deleteIndex($images)
+    {
+        $directory = public_path($images['directory']);
+        $this->deleteDirectoryAndFiles($directory);
+    }
+
+    public function deleteDirectoryAndFiles($directory)
+    {
+        if (!is_dir($directory)) {
+            return false;
+        }
+
+        $files = glob($directory . DIRECTORY_SEPARATOR . '*' . GLOB_MARK);
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                $this->deleteDirectoryAndFiles($file);
+            } else {
+                unlink($file);
+            }
+        }
+        $result = rmdir($directory);
+        return $result;
+    }
 }
