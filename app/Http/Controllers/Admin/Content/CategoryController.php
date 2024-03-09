1552fc04a3c87378b1a3e4cde00ad288dcf12a3c
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-10 01:24:51 +0330

    controller update image settings

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -84,10 +84,29 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(PostCategoryRequest $request, PostCategory $postCategory)
+    public function update(PostCategoryRequest $request, PostCategory $postCategory, ImageService $imageService)
     {
         $inputs = $request->all();
-        $inputs['image'] = 'image';
+        if ($request->hasFile('image')) {
+            if (!empty($postCategory->image)) {
+                $this->deleteDirectoryAndFiles($postCategory->image['directory']);
+            }
+
+            $imageService->setExclusiveDirectory('image' . DIRECTORY_SEPARATOR . 'post-category');
+
+            $result = $imageService->createIndexAndSave($request->file('image'));
+            if ($result === false) {
+                return redirect()->route('admin.content.category.index')->with('swal-error', 'عکس شما آپلود نشد');
+            }
+            $inputs['image'] = $result;
+        } else {
+            if (isset($inputs['currentImage']) && !empty($postCategory->image)) {
+                $image = $postCategory->image;
+                $image['currentImage'] = $inputs['currentImage'];
+                $inputs['image'] = $image;
+            }
+        }
+
         $postCategory->update($inputs);
         return redirect()->route('admin.content.category.index')->with('swal-success', 'دسته بندی شما با موفقیت ویرایش شد');;
     }
