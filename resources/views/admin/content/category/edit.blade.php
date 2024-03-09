a30ee59a6b227566fea990f7579ab930fd17f9a4
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-10 01:06:45 +0330

    add change currentImage in edit

--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -30,7 +30,7 @@
 
                 <section>
                     <form action="{{ route('admin.content.category.update', $postCategory->id) }}" method="post"
-                        enctype="multipart/form-data">
+                        enctype="multipart/form-data" id="form">
                         @csrf
                         {{ method_field('put') }}
                         <section class="row">
@@ -53,8 +53,11 @@
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
-                                    <input type="text" class="form-control form-control-sm" name="tags" id="tags"
+                                    <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
                                         value="{{ old('tags', $postCategory->tags) }}">
+                                    <select class="select2 form-control form-control-sm" id="select_tags" multiple>
+
+                                    </select>
                                 </div>
                                 @error('tags')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -100,33 +103,57 @@
                                 @enderror
                             </section>
 
+                            <section class="row">
+                                @php
+                                    $number = 1;
+                                @endphp
+                                @foreach ($postCategory->image['indexArray'] as $key => $value)
+                                    <section class="col-md-{{ 6 / $number }}">
+                                        <div class="form-check">
+                                            <input type="radio" class="form-check-input" name="currentImage"
+                                                value="{{ $key }}" id="{{ $number }}"
+                                                @if ($postCategory->image['currentImage'] == $key) checked @endif>
+                                            <label for="{{ $number }}" class="form-check-label mx-2">
+                                                <img src="{{ asset($value) }}" class="w-100" alt="">
+                                            </label>
+                                        </div>
+                                    </section>
+                                    @php
+                                        $number++;
+                                    @endphp
+                                @endforeach
 
-                            <section class="col-12">
-                                <div class="form-group">
-                                    <label for="">توضیحات</label>
-                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6">
-                                    {{ old('description', $postCategory->description) }}
-                                </textarea>
-                                </div>
-                                @error('description')
-                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                        <strong>
-                                            {{ $message }}
-                                        </strong>
-                                    </span>
-                                @enderror
                             </section>
 
-
-                            <section class="col-12 my-3">
-                                <button class="btn btn-primary btn-sm">ثبت</button>
-                            </section>
                         </section>
-                    </form>
-                </section>
 
+
+                        <section class="col-12">
+                            <div class="form-group">
+                                <label for="">توضیحات</label>
+                                <textarea name="description" id="description" class="form-control form-control-sm" rows="6">
+                                        {{ old('description', $postCategory->description) }}
+                                    </textarea>
+                            </div>
+                            @error('description')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{ $message }}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+
+                        <section class="col-12 my-3">
+                            <button class="btn btn-primary btn-sm">ثبت</button>
+                        </section>
+                </section>
+                </form>
             </section>
+
         </section>
+    </section>
     </section>
 @endsection
 
@@ -134,5 +161,32 @@
     <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('description');
+    </script>
+    <script>
+        $(document).ready(function() {
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if (tags_input.val() !== null && tags_input.val().length > 0) {
+                default_data = default_tags.split(',');
+            }
+
+            select_tags.select2({
+                placeholder: 'لطفا تگ های خود را وارد نمایید',
+                tags: true,
+                data: default_data
+            });
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function(event) {
+                if (select_tags.val() !== null && select_tags.val().length > 0) {
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource)
+                }
+            })
+        })
     </script>
 @endsection
