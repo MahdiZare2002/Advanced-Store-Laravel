00c0d5e7ee09745603e772a30e57d08adb4440e2
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-14 01:53:08 +0330

    make comment model

---
+++ app/Models/Content/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Content;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    
+}
