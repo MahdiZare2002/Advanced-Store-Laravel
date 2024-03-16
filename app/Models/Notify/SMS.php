33f6a6565ed1df249d44019ea519f7dd201f45fc
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-16 12:54:50 +0330

    create SMS model

---
+++ app/Models/Notify/SMS.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Notify;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SMS extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'public_sms';
+
+
+}
