579c872e4450c0423af84f3ee8ddfdee8af9299c
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-17 13:18:40 +0330

    complete setting part

--- app/Http/Controllers/Admin/Setting/SettingController.php
+++ app/Http/Controllers/Admin/Setting/SettingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Setting;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Setting\SettingRequest;
+use App\Http\Services\Image\ImageService;
 use App\Models\Setting\Setting;
 use Database\Seeders\SettingSeeder;
 use Illuminate\Http\Request;
@@ -25,37 +27,6 @@ class SettingController extends Controller
         return view('admin.setting.index', compact('setting'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // return view('admin.setting.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show()
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -75,19 +46,36 @@ class SettingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(SettingRequest $request, Setting $setting, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+
+        if ($request->hasFile('logo')) {
+            if (!empty($setting->logo)) {
+                $imageService->deleteImage($setting->logo);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'setting');
+            $imageService->setImageName('logo');
+            $result = $imageService->save($request->file('logo'));
+            if ($result === false) {
+                return redirect()->route('admin.content.category.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+            $inputs['logo'] = $result;
+        }
+        if ($request->hasFile('icon')) {
+            if (!empty($setting->icon)) {
+                $imageService->deleteImage($setting->icon);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'setting');
+            $imageService->setImageName('icon');
+            $result = $imageService->save($request->file('icon'));
+            if ($result === false) {
+                return redirect()->route('admin.content.category.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+            $inputs['icon'] = $result;
+        }
+        $setting->update($inputs);
+        return redirect()->route('admin.setting.index')->with('swal-success', 'تنظیمات سایت  شما با موفقیت ویرایش شد');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
