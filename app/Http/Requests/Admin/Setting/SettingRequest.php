b406c65d3ff6af939da9474350511bb45df99c56
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-17 12:35:40 +0330

    add Setting Request

---
+++ app/Http/Requests/Admin/Setting/SettingRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin\Setting;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'description' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'keywords' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'logo' => 'image|mimes:png,jpg,jpeg,gif',
+            'icon' => 'image|mimes:png,jpg,jpeg,gif',
+        ];
+    }
+}
