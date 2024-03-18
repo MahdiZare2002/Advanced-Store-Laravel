afa6d806865db4bd66135b3aa98cbc0761addd95
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-18 20:43:31 +0330

    admin-user status & activation

--- app/Http/Controllers/Admin/User/AdminUserController.php
+++ app/Http/Controllers/Admin/User/AdminUserController.php
@@ -90,4 +90,35 @@ class AdminUserController extends Controller
     {
         //
     }
+
+    public function status(User $user)
+    {
+        $user->status = $user->status == 0 ? 1 : 0;
+        $result = $user->save();
+        if ($result) {
+            if ($user->status == 0) {
+                return response()->json(['status' => true, 'checked' => false]);
+            } else {
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        } else {
+            return response()->json(['status' => false]);
+        }
+    }
+
+    
+    public function activation(User $user)
+    {
+        $user->status = $user->status == 0 ? 1 : 0;
+        $result = $user->save();
+        if ($result) {
+            if ($user->status == 0) {
+                return response()->json(['status' => true, 'checked' => false]);
+            } else {
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        } else {
+            return response()->json(['status' => false]);
+        }
+    }
 }
