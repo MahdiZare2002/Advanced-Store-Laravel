dce50be154208ca55318bc504a3e05695996531a
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-19 01:34:31 +0330

    Ticket Parts Ui pages

---
+++ resources/views/admin/ticket/category/edit.blade.php
@@ -0,0 +1,89 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>دسته بندی</title>
+@endsection
+
+@section('content')
+
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش تیکت ها</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش دسته بندی</li>
+        </ol>
+    </nav>
+
+
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header">
+                    <h5>
+                        ویرایش دسته بندی
+                    </h5>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                    <a href="{{ route('admin.ticket.category.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                </section>
+
+                <section>
+
+                    <form action="{{ route('admin.ticket.category.update', $ticketCategory->id) }}" method="post">
+                        @csrf
+                        {{ method_field('put') }}
+                        <section class="row">
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="name">نام دسته</label>
+                                    <input type="text" class="form-control form-control-sm" name="name" id="name"
+                                        value="{{ old('name', $ticketCategory->name) }}">
+                                </div>
+                                @error('name')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select name="status" id="" class="form-control form-control-sm" id="status">
+                                        <option value="0" @if (old('status', $ticketCategory->status) == 0) selected @endif>غیرفعال</option>
+                                        <option value="1" @if (old('status', $ticketCategory->status) == 1) selected @endif>فعال</option>
+                                    </select>
+                                </div>
+                                @error('status')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+                            </section>
+
+
+                            <section class="col-12">
+
+                            <section class="col-12 my-3">
+                                <button class="btn btn-primary btn-sm">ثبت</button>
+                            </section>
+                        </section>
+                    </form>
+                </section>
+
+            </section>
+        </section>
+    </section>
+
+@endsection
---
+++ resources/views/admin/ticket/category/index.blade.php
@@ -0,0 +1,156 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+<title>دسته بندی</title>
+@endsection
+
+@section('content')
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">بخش تیکت ها</a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
+    </ol>
+  </nav>
+
+
+  <section class="row">
+    <section class="col-12">
+        <section class="main-body-container">
+            <section class="main-body-container-header">
+                <h5>
+                  دسته بندی
+                </h5>
+            </section>
+
+            @include('admin.alerts.alert-section.success')
+
+            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                <a href="{{ route('admin.ticket.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
+                <div class="max-width-16-rem">
+                    <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
+                </div>
+            </section>
+
+            <section class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>نام دسته بندی</th>
+                            <th>وضعیت</th>
+                            <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($ticketCategories as $key => $ticketCategory)
+
+                        <tr>
+                            <th>{{ $key += 1 }}</th>
+                            <td>{{ $ticketCategory->name }}</td>
+                            <td>
+                                <label>
+                                    <input id="{{ $ticketCategory->id }}" onchange="changeStatus({{ $ticketCategory->id }})" data-url="{{ route('admin.ticket.category.status', $ticketCategory->id) }}" type="checkbox" @if ($ticketCategory->status === 1)
+                                    checked
+                                    @endif>
+                                </label>
+                            </td>
+                            <td class="width-16-rem text-left">
+                                <a href="{{ route('admin.ticket.category.edit', $ticketCategory->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
+                                <form class="d-inline" action="{{ route('admin.ticket.category.destroy', $ticketCategory->id) }}" method="post">
+                                    @csrf
+                                    {{ method_field('delete') }}
+                                <button class="btn btn-danger btn-sm delete" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
+                            </form>
+                            </td>
+                        </tr>
+
+                        @endforeach
+
+
+                    </tbody>
+                </table>
+            </section>
+
+        </section>
+    </section>
+</section>
+
+@endsection
+@section('script')
+
+    <script type="text/javascript">
+        function changeStatus(id){
+            var element = $("#" + id)
+            var url = element.attr('data-url')
+            var elementValue = !element.prop('checked');
+
+
+            $.ajax({
+                url : url,
+                type : "GET",
+                success : function(response){
+                    if(response.status){
+                        if(response.checked){
+                            element.prop('checked', true);
+                            successToast('دسته بندی با موفقیت فعال شد')
+                        }
+                        else{
+                            element.prop('checked', false);
+                            successToast('دسته بندی با موفقیت غیر فعال شد')
+                        }
+                    }
+                    else{
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error : function(){
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            });
+
+            function successToast(message){
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(successToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+
+            function errorToast(message){
+
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(errorToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+        }
+    </script>
+
+
+@include('admin.alerts.sweetalert.delete-confirm', ['className' => 'delete'])
+
+
+@endsection
