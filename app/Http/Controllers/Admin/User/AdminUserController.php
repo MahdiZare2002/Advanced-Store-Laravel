69707a8c3075f0316ad7f80f08e3258ad6ab7a1c
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-18 23:01:24 +0330

    complete admin-user part

--- app/Http/Controllers/Admin/User/AdminUserController.php
+++ app/Http/Controllers/Admin/User/AdminUserController.php
@@ -42,13 +42,12 @@ class AdminUserController extends Controller
     {
         $inputs = $request->all();
         if ($request->hasFile('profile_photo_path')) {
-            $imageService->getExclusiveDirectory('images', DIRECTORY_SEPARATOR, 'users');
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'users');
             $result = $imageService->save($request->file('profile_photo_path'));
 
             if ($result === false) {
                 return redirect()->route('admin.user.admin-user.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
             }
-
             $inputs['profile_photo_path'] = $result;
         }
         $inputs['password'] = Hash::make($request->password);
@@ -56,16 +55,15 @@ class AdminUserController extends Controller
         $user = User::create($inputs);
         return redirect()->route('admin.user.admin-user.index')->with('swal-success', 'ادمین جدید با موفقیت ثبت شد');
     }
-
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(User $user)
+    public function edit(User $admin)
     {
-        return view('admin.user.admin-user.edit', compact('user'));
+        return view('admin.user.admin-user.edit', compact('admin'));
     }
 
     /**
@@ -75,9 +73,23 @@ class AdminUserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(AdminUserRequest $request, User $admin, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+
+        if ($request->hasFile('profile_photo_path')) {
+            if (!empty($admin->profile_photo_path)) {
+                $imageService->deleteImage($admin->profile_photo_path);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'users');
+            $result = $imageService->save($request->file('profile_photo_path'));
+            if ($result === false) {
+                return redirect()->route('admin.user.admin-user.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+            $inputs['profile_photo_path'] = $result;
+        }
+        $admin->update($inputs);
+        return redirect()->route('admin.user.admin-user.index')->with('swal-success', 'ادمین سایت شما با موفقیت ویرایش شد');
     }
 
     /**
@@ -86,9 +98,10 @@ class AdminUserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $admin)
     {
-        //
+        $result = $admin->forceDelete();
+        return redirect()->route('admin.user.admin-user.index')->with('swal-success', 'ادمین شما با موفقیت حذف شد');
     }
 
     public function status(User $user)
@@ -106,7 +119,7 @@ class AdminUserController extends Controller
         }
     }
 
-    
+
     public function activation(User $user)
     {
         $user->status = $user->status == 0 ? 1 : 0;
--- routes/web.php
+++ routes/web.php
@@ -263,11 +263,11 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
         Route::prefix('admin-user')->group(function () {
 
             Route::get('/', [AdminUserController::class, 'index'])->name('admin.user.admin-user.index');
-            Route::get('/create', [AdminUserController::class, 'show'])->name('admin.user.admin-user.create');
+            Route::get('/create', [AdminUserController::class, 'create'])->name('admin.user.admin-user.create');
             Route::post('/store', [AdminUserController::class, 'store'])->name('admin.user.admin-user.store');
-            Route::get('/edit/{user}', [AdminUserController::class, 'edit'])->name('admin.user.admin-user.edit');
-            Route::put('/update/{user}', [AdminUserController::class, 'update'])->name('admin.user.admin-user.update');
-            Route::delete('/destroy/{user}', [AdminUserController::class, 'destroy'])->name('admin.user.admin-user.destroy');
+            Route::get('/edit/{admin}', [AdminUserController::class, 'edit'])->name('admin.user.admin-user.edit');
+            Route::put('/update/{admin}', [AdminUserController::class, 'update'])->name('admin.user.admin-user.update');
+            Route::delete('/destroy/{admin}', [AdminUserController::class, 'destroy'])->name('admin.user.admin-user.destroy');
             Route::get('/status/{user}', [AdminUserController::class, 'status'])->name('admin.user.admin-user.status');
             Route::get('/activation/{user}', [AdminUserController::class, 'activation'])->name('admin.user.admin-user.activation');
         });
