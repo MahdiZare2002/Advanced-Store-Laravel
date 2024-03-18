14726b7c5c4b35e3160692d59eb9b7bbdc38abae
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-19 01:36:31 +0330

    Ticket parts models

---
+++ app/Models/Ticket/Ticket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Ticket;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Ticket/TicketCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Ticket;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TicketCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'status'];
+
+}
