d1c6df1be3d846cdcaa6bce5fb5202faf802542a
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-19 01:37:42 +0330

    Ticket parts requests

---
+++ app/Http/Requests/Admin/Ticket/TicketCategoryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Admin\Ticket;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TicketCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'status' => 'required|numeric|in:0,1',
+        ];
+    }
+}
