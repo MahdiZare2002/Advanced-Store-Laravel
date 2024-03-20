d8fd5f0d1e6f89dcfc11f134ae6f3a8e0b2da08d
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-20 15:46:28 +0330

    store role

--- app/Http/Controllers/Admin/User/RoleController.php
+++ app/Http/Controllers/Admin/User/RoleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\User;
 use App\Models\User\Role;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\User\RoleRequest;
 use App\Models\User\Permission;
 
 class RoleController extends Controller
@@ -37,9 +38,13 @@ class RoleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(RoleRequest $request)
     {
-        //
+        $inputs = $request->all();
+        $role = Role::create($inputs);
+        $inputs['permissions'] = $inputs['permissions'] ?? [];
+        $role->permissions()->sync($inputs['permissions']);
+        return redirect()->route('admin.user.role.index')->with('swal-success', 'نقش جدید با موفقیت ثبت شد');
     }
 
     /**
