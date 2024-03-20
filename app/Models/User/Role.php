6fc82bf4084c890aaec3cc7df18bd1f4448824d8
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-20 14:48:17 +0330

    create role and permission models

--- app/Models/User/Role.php
+++ app/Models/User/Role.php
@@ -2,10 +2,23 @@
 
 namespace App\Models\User;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\User\Permission;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'status'];
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+    
+    public function permissions(){
+        return $this->belongsToMany(Permission::class);
+    }
 }
