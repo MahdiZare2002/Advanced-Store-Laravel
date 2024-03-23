1e1f41c07bb8ba2d47c317c888cc21e278829d92
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-24 02:25:56 +0330

    make user register notification message

--- app/Http/Controllers/Admin/User/CustomerController.php
+++ app/Http/Controllers/Admin/User/CustomerController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Services\Image\ImageService;
 use App\Http\Requests\Admin\User\CustomerRequest;
+use App\Notifications\NewUserRegistered;
 
 class CustomerController extends Controller
 {
@@ -53,6 +54,11 @@ class CustomerController extends Controller
         $inputs['password'] = Hash::make($request->password);
         $inputs['user_type'] = 0;
         $user = User::create($inputs);
+        $adminUser = User::find(1);
+        $details = [
+            "message" => 'یک کاربر جدید در سایت ثبت نام کرد',
+        ];
+        $adminUser->notify(new NewUserRegistered($details));
         return redirect()->route('admin.user.customer.index')->with('swal-success', 'مشتری جدید با موفقیت ثبت شد');
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Content\Comment;
+use App\Models\Notification;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('admin.layouts.header' , function($view){
+            view()->with('unseenComments', Comment::where('seen', 0)->get());
+            view()->with('notifications', Notification::where('read_at', null)->get());
+        });
     }
 }
