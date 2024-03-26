523519abebf783a554b6b251c585f2a0ea736288
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-26 04:31:09 +0330

    add some useful helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -7,3 +7,64 @@ function jalaliDate($date, $format = '%A, %d %B %Y')
 {
     return Jalalian::forge($date)->format($format);
 }
+
+
+function convertPersianToEnglish($number)
+{
+    $number = str_replace('۰', '0', $number);
+    $number = str_replace('۱', '1', $number);
+    $number = str_replace('۲', '2', $number);
+    $number = str_replace('۳', '3', $number);
+    $number = str_replace('۴', '4', $number);
+    $number = str_replace('۵', '5', $number);
+    $number = str_replace('۶', '6', $number);
+    $number = str_replace('۷', '7', $number);
+    $number = str_replace('۸', '8', $number);
+    $number = str_replace('۹', '9', $number);
+
+    return $number;
+}
+
+
+function convertArabicToEnglish($number)
+{
+    $number = str_replace('۰', '0', $number);
+    $number = str_replace('۱', '1', $number);
+    $number = str_replace('۲', '2', $number);
+    $number = str_replace('۳', '3', $number);
+    $number = str_replace('۴', '4', $number);
+    $number = str_replace('۵', '5', $number);
+    $number = str_replace('۶', '6', $number);
+    $number = str_replace('۷', '7', $number);
+    $number = str_replace('۸', '8', $number);
+    $number = str_replace('۹', '9', $number);
+
+    return $number;
+}
+
+
+
+function convertEnglishToPersian($number)
+{
+    $number = str_replace('0', '۰', $number);
+    $number = str_replace('1', '۱', $number);
+    $number = str_replace('2', '۲', $number);
+    $number = str_replace('3', '۳', $number);
+    $number = str_replace('4', '۴', $number);
+    $number = str_replace('5', '۵', $number);
+    $number = str_replace('6', '۶', $number);
+    $number = str_replace('7', '۷', $number);
+    $number = str_replace('8', '۸', $number);
+    $number = str_replace('9', '۹', $number);
+
+    return $number;
+}
+
+
+
+function priceFormat($price)
+{
+    $price = number_format($price, 0, '/', '،');
+    $price = convertEnglishToPersian($price);
+    return $price;
+}
