5e29076f6bffb7486b991d01868fb9ef85da81ef
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-26 04:00:21 +0330

    create logout option

--- routes/web.php
+++ routes/web.php
@@ -418,6 +418,8 @@ Route::namespace('auth')->group(function () {
     Route::get('login-confirm/{token}', [LoginRegisterController::class, 'loginConfirmForm'])->name('auth.customer.login-confirm-form');
     Route::middleware('throttle:customer-login-confirm-limiter')->post('/login-confirm/{token}', [LoginRegisterController::class, 'loginConfirm'])->name('auth.customer.login-confirm');
     Route::middleware('throttle:customer-login-resend-otp-limiter')->get('/login-resend-otp/{token}', [LoginRegisterController::class, 'loginResendOtp'])->name('auth.customer.login-resend-otp');
+    Route::get('/logout', [LoginRegisterController::class, 'logout'])->name('auth.customer.logout');
+
 });
 
 Route::get('/', function () {
