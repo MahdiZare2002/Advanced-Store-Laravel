6c7f361dbbc664418c1a8437a07364c8fc810e22
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-03-29 05:17:43 +0330

    create product page controller

---
+++ app/Http/Controllers/Customer/Market/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Customer\Market;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+}
