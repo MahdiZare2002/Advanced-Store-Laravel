55349cfcaf74f33770d9044bf6a1e80905c5e89c
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-01 01:54:32 +0330

    send cartItems to header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use App\Models\Content\Comment;
 use App\Models\Notification;
+use App\Models\Content\Comment;
+use App\Models\Market\CartItem;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +30,13 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('admin.layouts.header', function ($view) {
             $view->with('unseenComments', Comment::where('seen', 0)->get());
             $view->with('notifications', Notification::where('read_at', null)->get());
+        });
+
+        view()->composer('customer.layouts.header', function ($view) {
+            if (Auth::check()) {
+                $cartItems = CartItem::where('user_id', Auth::user()->id)->get();
+                $view->with('cartItems', $cartItems);
+            }
         });
     }
 }
