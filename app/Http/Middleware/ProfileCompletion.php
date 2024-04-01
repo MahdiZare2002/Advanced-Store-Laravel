89080d0e6abe7aa5b92c36fe6c62402fc8e96d91
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-01 12:53:53 +0330

    handle auth in ProfileCompletion middleware

--- app/Http/Middleware/ProfileCompletion.php
+++ app/Http/Middleware/ProfileCompletion.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileCompletion
 {
@@ -16,6 +17,18 @@ class ProfileCompletion
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!empty(Auth::user()->email) && empty(Auth::user()->mobile) && empty(Auth::user()->email_verified_at)) {
+            return redirect()->route('customer.sales-process.profile-completion');
+        }
+
+        if (empty(Auth::user()->first_name) || empty(Auth::user()->last_name) || empty(Auth::user()->national_code)) {
+            return redirect()->route('customer.sales-process.profile-completion');
+        }
+
+        if (!empty(Auth::user()->mobile) && empty(Auth::user()->email) && empty(Auth::user()->mobile_verified_at)) {
+            return redirect()->route('customer.sales-process.profile-completion');
+        }
+
         return $next($request);
     }
 }
