0658a8e1a510d7e25f37e3777ba7abadbc775425
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-01 12:45:26 +0330

    begin address and profile complete

--- app/Http/Controllers/Customer/SalesProcess/AddressController.php
+++ app/Http/Controllers/Customer/SalesProcess/AddressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer\SalesProcess;
 
 use App\Http\Controllers\Controller;
+use App\Models\Market\CartItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,8 +13,14 @@ class AddressController extends Controller
     {
         $user = Auth::user();
 
-        if (empty($user->mobile) || empty($user->first_name) || empty($user->last_name) || empty($user->email) || empty($user->nation_code)) {
+        if (empty($user->mobile) || empty($user->first_name) || empty($user->last_name) || empty($user->email) || empty($user->national_code)) {
             return redirect()->route('customer.sales-process.profile-completion');
         }
+
+        if (empty(CartItem::where('user_id', $user->id)->count())) {
+            return redirect()->route('customer.sales-process.cart');
+        }
+
+        return view('customer.sales-process.delivery-and-address');
     }
 }
--- app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
+++ app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
@@ -16,8 +16,18 @@ class ProfileCompletionController extends Controller
         return view('customer.sales-process.profile-completion', compact('user', 'cartItems'));
     }
 
-    public function update()
+    public function update(Request $request)
     {
-
+        $request->validate([
+            'first_name' => 'sometimes|required',
+            'last_name' => 'sometimes|required',
+            'email' => 'email|unique:users,email',
+            'mobile' => 'sometimes|required|min:10|max:13||unique:users,mobile',
+            'national_code' => 'sometimes|required',
+        ]);
+        $user = Auth::user();
+        $inputs = $request->all();
+        $user->update($inputs);
+        return redirect()->route('customer.sales-process.address-and-delivery');
     }
 }
