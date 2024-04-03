843326658793bc6f56c42e6479a46e40d35ff033
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-03 09:18:39 +0330

    add province_id to addresses

---
+++ database/migrations/2024_04_03_090628_add_province_id_to_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProvinceIdToAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->foreignId('province_id')->after('city_id')->constrained('provinces')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -463,6 +463,7 @@ Route::namespace('salesProcess')->group(function () {
         //address
         Route::get('/address-and-delivery', [AddressController::class, 'addressAndDelivery'])->name('customer.sales-process.address-and-delivery');
         Route::post('/add-address', [AddressController::class, 'addAddress'])->name('customer.sales-process.add-address');
+        Route::put('/update-address/{address}', [AddressController::class, 'updateAddress'])->name('customer.sales-process.update-address');
         Route::get('/get-cities/{province}', [AddressController::class, 'getCities'])->name('customer.sales-process.get-cities');
     });
 });
