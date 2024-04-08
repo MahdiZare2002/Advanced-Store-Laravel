7286c26f48f5befd39b968e20721a2d923707539
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-08 22:34:24 +0330

    Ordering Payments

--- app/Http/Controllers/Customer/Profile/OrderController.php
+++ app/Http/Controllers/Customer/Profile/OrderController.php
@@ -9,6 +9,11 @@ class OrderController extends Controller
 {
     public function index()
     {
-        //
+        if (isset(request()->type)) {
+            $orders = auth()->user()->orders()->where('order_status', request()->type)->orderBy('id', 'desc')->get();
+        } else {
+            $orders = auth()->user()->orders()->orderBy('id', 'desc')->get();
+        }
+        return view('customer.profile.orders', compact('orders'));
     }
 }
