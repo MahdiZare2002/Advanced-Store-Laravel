3fe7e38088bd4a1cbb3064d1541f7c985a542f39
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-14 15:11:49 +0330

    Debug autoLoad => use helpers in blades

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -4,6 +4,19 @@
 @section('content')
     <!-- start slideshow -->
     <section class="container-xxl my-4">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @if (session('danger'))
+            <div class="alert alert-danger">
+                {{ session('danger') }}
+            </div>
+        @endif
+
+
         <section class="row">
             <section class="col-md-8 pe-md-1 ">
                 <section id="slideshow" class="owl-carousel owl-theme">
