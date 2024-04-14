95907c0209c052903db8cb7e759cd71c5841e9cf
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-15 02:20:29 +0330

    store ticket in user's profile

--- app/Http/Controllers/Customer/Profile/TicketController.php
+++ app/Http/Controllers/Customer/Profile/TicketController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Customer\Profile;
 
 use Illuminate\Http\Request;
 use App\Models\Ticket\Ticket;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\Ticket\TicketCategory;
 use App\Models\Ticket\TicketPriority;
+use App\Http\Services\File\FileService;
 use App\Http\Requests\Customer\Profile\StoreTicketRequest;
 use App\Http\Requests\Customer\Profile\StoreAnswerTicketRequest;
 
@@ -51,5 +53,34 @@ class TicketController extends Controller
         $ticketCategories = TicketCategory::all();
         $ticketPriorities = TicketPriority::all();
         return view('customer.profile.tickets.create', compact('ticketCategories', 'ticketPriorities'));
+    }
+
+    public function store(StoreTicketRequest $request, FileService $fileService)
+    {
+
+        DB::transaction(function () use ($request, $fileService) {
+            //ticket body
+            $inputs = $request->all();
+            $inputs['user_id'] = auth()->user()->id;
+            $ticket = Ticket::create($inputs);
+
+            //ticket file
+            if ($request->hasFile('file')) {
+                $fileService->setExclusiveDirectory('files' . DIRECTORY_SEPARATOR . 'ticket-files');
+                $fileService->setFileSize($request->file('file'));
+                $fileSize = $fileService->getFileSize();
+                $result = $fileService->moveToPublic($request->file('file'));
+                // $result = $fileService->moveToStorage($request->file('file'));
+                $fileFormat = $fileService->getFileFormat();
+                $inputs['ticket_id'] = $ticket->id;
+                $inputs['file_path'] = $result;
+                $inputs['file_size'] = $fileSize;
+                $inputs['file_type'] = $fileFormat;
+                $inputs['user_id'] = auth()->user()->id;
+                $file = TicketFile::create($inputs);
+            }
+        });
+
+        return redirect()->route('customer.profile.my-tickets')->with('swal-success', '  تیکت شما با موفقیت ثبت شد');
     }
 }
