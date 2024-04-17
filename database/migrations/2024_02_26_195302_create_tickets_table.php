b570c86c25b4210a09808873fe1fccbb32ee328d
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-17 11:08:49 +0330

    set reference_id nullable in tickets

--- database/migrations/2024_02_26_195302_create_tickets_table.php
+++ database/migrations/2024_02_26_195302_create_tickets_table.php
@@ -19,7 +19,7 @@ class CreateTicketsTable extends Migration
             $table->text('description');
             $table->tinyInteger('status')->default(0);
             $table->tinyInteger('seen')->default(0)->comment('0=>unseen , 1=>seen');
-            $table->foreignId('reference_id')->constrained('ticket_admins')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('reference_id')->nullable()->constrained('ticket_admins')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('category_id')->constrained('ticket_categories')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('priority_id')->constrained('ticket_priorities')->onUpdate('cascade')->onDelete('cascade');
