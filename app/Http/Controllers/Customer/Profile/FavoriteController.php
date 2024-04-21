0b45f18223be82308391dda21ca03d1965e778d2
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-04-21 15:03:13 +0330

    Debug => delete from favorite products

--- app/Http/Controllers/Customer/Profile/FavoriteController.php
+++ app/Http/Controllers/Customer/Profile/FavoriteController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Customer\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Models\Market\Product;
-use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
@@ -16,7 +15,7 @@ class FavoriteController extends Controller
     public function delete(Product $product)
     {
         $user = auth()->user();
-        $user->products->detach($product->id);
+        $user->products()->detach($product->id);
         return redirect()->route('customer.profile.my-favorites')->with('success', 'محصول با موفقیت از علاقه مندی ها حذف شد');
     }
 }
